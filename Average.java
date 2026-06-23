659d49860c2ff6a5c5acdb97f46a7b0f12af3d1a
Namrata Bharat Jadhav <namujadhav1717@gmail.com>
2026-06-23 19:35:19 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,12 @@
+ class Average {
+  public static void main(String[] args) {
+    double num1 = 70;
+    double num2 = 90;
+       double sum = 0.0;
+    double avg = 0.0;
+    sum = num1 + num2;
+    avg = sum/2;
+    System.out.println("Average: " + avg );
+  }
+}
+
