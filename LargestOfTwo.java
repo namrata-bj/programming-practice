7b1cfbfc36b76ddc58c9706948610e470276f472
Namrata Bharat Jadhav <namujadhav1717@gmail.com>
2026-08-01 20:18:11 +0530

    Add files via upload

---
+++ LargestOfTwo.java
@@ -0,0 +1,11 @@
+class LargestTwo {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        if(a > b) {
+            System.out.println("A is largest");
+        } else {
+            System.out.println("B is largest");
+        }
+    }
+}
